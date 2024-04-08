9c5dd34c77f2f1509d10cad1c1e266a28deeff50
TzRPheonix <tzrpheonix@gmail.com>
2024-04-08 17:29:53 +0200

    Ajout du mécanisme de combat et de fin de jeu

--- FrontEnd/mmorpg/src/fight/fight.js
+++ FrontEnd/mmorpg/src/fight/fight.js
@@ -1,50 +1,161 @@
 import './fight.css'; 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import LifeBar from './lifeBar/lifeBar';
+import './fight.css'; 
+
+async function loadUserInfo() {
+    try {
+        const username = localStorage.getItem('username');
+        console.log(username);
+        const response = await fetch(`http://localhost:3000/api/getUser/${username}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function getRandomMonster() {
+    try {
+        const username = localStorage.getItem('username');
+        console.log(username);
+        const response = await fetch(`http://localhost:3000/api/getRandomMonster/${username}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+async function handleEndCombat(starterPV, monsterPV) {
+    const username = localStorage.getItem('username');
+
+    try {
+        const response = await fetch('http://localhost:3000/api/EndCombat', {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ starterPV, monsterPV, username })
+        });
+
+        const data = await response.json();
+        console.log(data.message);
+        window.location.href = 'http://localhost:3001/Fight';
+    } catch (error) {
+        console.error(error);
+    }
+}
+
 function Fight() {
     const playerName = localStorage.getItem('username');
-    const [userData, setUserData] = useState(null);
+    const [StarterLVL, setStarterLVL] = useState(null);
+    const [StarterPV, setStarterPV] = useState(null);
+    const [StarterMAXPV, setStarterMAXPV] = useState(null);
+    const [StarterDMG, setStarterDMG] = useState(null);
+    const [monsterName, setMonsterName] = useState(null);
+    const [monsterPV, setMonsterPV] = useState(null);
+    const [monsterMAXPV, setMonsterMAXPV] = useState(null);
+    const [monsterDMG, setMonsterDMG] = useState(null);
 
     useEffect(() => {
         const fetchData = async () => {
             const data = await loadUserInfo();
-            setUserData(data);
+            setStarterLVL(data.starterLVL);
+            setStarterPV(data.starterPV);
+            setStarterMAXPV(data.starterMAXPV);
+            setStarterDMG(data.starterDMG);
+        };
+
+        fetchData();
+    }, []);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const data = await getRandomMonster();
+            console.log(data)
+            setMonsterName(data.monstername);
+            setMonsterPV(data.monsterPV);
+            setMonsterMAXPV(data.monsterPV);
+            setMonsterDMG(data.monsterDMG);
         };
 
         fetchData();
     }, []);
 
     const handleAttack = () => {
-        console.log('Attack !');
-      };
+        const newMonsterPV = monsterPV - StarterDMG;
+        if (newMonsterPV < 0) {
+            setMonsterPV(0);
+        } else {
+            setMonsterPV(newMonsterPV);
+        }
+        if (newMonsterPV <= 0 || StarterPV <= 0) {
+            handleEndCombat(StarterPV, newMonsterPV);
+        }
+    };
+
+    const handleDamage = () => {
+        const newPlayerPV = StarterPV - monsterDMG;
+        if (newPlayerPV < 0) {
+            setStarterPV(0);
+        } else {
+            setStarterPV(newPlayerPV);
+        }
+        if (monsterPV <= 0 || StarterPV <= 0) {
+            handleEndCombat(StarterPV, monsterPV);
+        }
+    };
+
+    // useEffect(() => {
+    //     const interval = setInterval(() => {
+    //         handleDamage();
+    //     }, 5000);
+
+    //     return () => {
+    //         clearInterval(interval);
+    //     };
+    // }, []);
+
   return (
     <div className="fight">
         <div className='HeaderFight'>
             <button style={{visibility:'hidden'}}>Leave </button>
             <div className='title-container'>
-                <span className='title' style={{color:"#9B1D20"}}>Level 18</span>
+                <span className='title' style={{color:"#9B1D20"}}>{StarterLVL}</span>
             </div>
             <button className='leaveFight'>Leave </button>
         </div>
         <div className='containerFight'>
             <div className='fightZone'>
                 <div className='playerZone'>
-                    <span>playerName</span>
+                    <span>{playerName}</span>
                     <div className='lifeBar'>
                     <LifeBar health={75} />
                     </div>
-                    <span style={{color:'#F4B860'}}>PV : 100</span>
+                    <span style={{color:'#F4B860'}}>PV : {StarterPV}/{StarterMAXPV}</span>
                     <br></br>
-                    <span style={{color:'#F4B860'}}>Damage : 2</span>
+                    <span style={{color:'#F4B860'}}>Damage : {StarterDMG}</span>
                 </div>
                 <div className='monsterZone'>
-                    <span >monsterName</span>
+                    <span >{monsterName}</span>
                     <div className='lifeBar'>
                     <LifeBar health={100} />
                     </div>
-                    <span style={{color:'#F4B860'}}>PV : 150</span>
+                    <span style={{color:'#F4B860'}}>PV : {monsterPV}/{monsterMAXPV}</span>
                     <br></br>
-                    <span style={{color:'#F4B860'}}>Damage : 1</span>
+                    <span style={{color:'#F4B860'}}>Damage : {monsterDMG}</span>
                 </div>
             </div>
             <div className='attack'>
