f388c4e4a63445c770a18ba34e98682ec47581c8
TzRPheonix <tzrpheonix@gmail.com>
2024-04-08 15:10:03 +0200

    clean + ajout du router

--- FrontEnd/mmorpg/src/App.js
+++ FrontEnd/mmorpg/src/App.js
@@ -1,13 +1,21 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Login from './login/login.js';
 import Register from './register/register.js';
 import ChoiceStarter from './choiceStarter/choiceStarter.js'
-function App() {
+
+export default function App() {
   return (
-    <div className="App">
-      <ChoiceStarter></ChoiceStarter>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/choiceStarter" element={<ChoiceStarter />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
-export default App;
+createRoot(document.getElementById('root')).render(<App />);
