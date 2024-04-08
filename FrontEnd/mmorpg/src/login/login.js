beedc905b1c2a78774e8a28a9391f505aef57e8a
thomas <cornu.thomas6@gmail.com>
2024-04-08 13:00:47 +0200

    create file login and register in front

---
+++ FrontEnd/mmorpg/src/login/login.js
@@ -0,0 +1,16 @@
+import './login.css';
+
+function Login() {
+  return (
+    <div className="App">
+        <div className="container">
+            <div className="left-side"></div>
+            <div className="right-side">
+                <span>Login</span>
+            </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
---
+++ FrontEnd/mmorpg/src/register/register.js
@@ -0,0 +1,16 @@
+import './register.css';
+
+function Register() {
+  return (
+    <div className="App">
+        <div className="container">
+            <div className="left-side"></div>
+            <div className="right-side">
+                <span>Register</span>
+            </div>
+      </div>
+    </div>
+  );
+}
+
+export default Register;
