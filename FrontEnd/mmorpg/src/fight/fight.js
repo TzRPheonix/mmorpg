01c564b10d34d1e7de30bdad76e9e4664fc5d613
thomas <cornu.thomas6@gmail.com>
2024-04-08 16:51:26 +0200

    design fight

--- FrontEnd/mmorpg/src/fight/fight.js
+++ FrontEnd/mmorpg/src/fight/fight.js
@@ -1,22 +1,43 @@
 import './fight.css'; 
-
+import React, { useState } from 'react';
+import LifeBar from './lifeBar/lifeBar';
 function Fight() {
 
+    const handleAttack = () => {
+        console.log('Attack !');
+      };
   return (
     <div className="fight">
         <div className='HeaderFight'>
-            <button style={{display:'none'}}>Leave </button>
+            <button style={{visibility:'hidden'}}>Leave </button>
             <div className='title-container'>
                 <span className='title' style={{color:"#9B1D20"}}>Level 18</span>
             </div>
             <button className='leaveFight'>Leave </button>
         </div>
-        <div className='fightZone'>
-            <div className='playerZone'>
-                <span className='playerName'>Nom du Joueur</span>
+        <div className='containerFight'>
+            <div className='fightZone'>
+                <div className='playerZone'>
+                    <span>playerName</span>
+                    <div className='lifeBar'>
+                    <LifeBar health={75} />
+                    </div>
+                    <span style={{color:'#F4B860'}}>PV : 100</span>
+                    <br></br>
+                    <span style={{color:'#F4B860'}}>Damage : 2</span>
+                </div>
+                <div className='monsterZone'>
+                    <span >monsterName</span>
+                    <div className='lifeBar'>
+                    <LifeBar health={100} />
+                    </div>
+                    <span style={{color:'#F4B860'}}>PV : 150</span>
+                    <br></br>
+                    <span style={{color:'#F4B860'}}>Damage : 1</span>
+                </div>
             </div>
-            <div className='monsterZone'>
-                <span className='monsterName'>Nom du Monstre</span>
+            <div className='attack'>
+                <button className='btnAttack' onClick={handleAttack}>Attack</button>
             </div>
         </div>
     </div>
