b8c8f4904c0fd9ae48a18a5f841d4cd75d2dad78
TzRPheonix <tzrpheonix@gmail.com>
2024-04-23 09:33:32 +0200

    back potion

--- BackEnd/routes/route.js
+++ BackEnd/routes/route.js
@@ -172,7 +172,7 @@ router.get('/getRandomMonster/:username', async (req, res) => {
 
 // Put Method for changing stats after combat
 router.put('/EndCombat', async (req, res) => {
-  const { starterPV, monsterPV, username } = req.body;
+  const { starterPV, monsterPV, username, nbPotion } = req.body;
   try {
     const existingUser = await userModel.findOne({ username });
     if (!existingUser) {
@@ -188,13 +188,16 @@ router.put('/EndCombat', async (req, res) => {
       return res.status(200).json({ message: 'Vous avez leave.' });
     }
     
+    existingUser.healthPotionCount = nbPotion;
+
     if (Math.random() < 0.2) {
       existingUser.healthPotionCount += 1;
     }
+
     if (starterPV < existingUser.starterPV) {
       existingUser.starterPV = Math.round(starterPV + ((existingUser.starterPV - starterPV) * 0.5))+ 10;
     }else {
-      existingUser.starterPV += 10;
+      existingUser.starterPV = starterPV + 10;
     }
     existingUser.starterMAXPV += 10;
     existingUser.starterDMG += 1;
@@ -204,36 +207,6 @@ router.put('/EndCombat', async (req, res) => {
     await existingUser.save();
 
     res.status(200).json({ message: 'Gagné ! LVL augmenté.'});
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-});
-
-router.put('/useHealthPotion', async (req, res) => {
-  const { username, StarterPV } = req.body;
-  console.log(StarterPV)
-  try {
-    const existingUser = await userModel.findOne({ username });
-    if (!existingUser) {
-      return res.status(400).json({ message: 'Utilisateur introuvable.' });
-    }
-    if (StarterPV == 0) {
-      console.log('Vous êtes mort.')
-      return res.status(200).json({ message: 'Vous êtes mort.', newPV: 0});
-    }
-    if (existingUser.healthPotionCount <= 0) {
-      console.log('Pas de potion de soin.')
-      return res.status(200).json({ message: 'Pas de potion de soin.', newPV: StarterPV });
-    }
-    if (StarterPV == existingUser.starterMAXPV) {
-      console.log('Déjà en pleine santé!')
-      return res.status(200).json({ message: 'Déjà en pleine santé!', newPV: StarterPV });
-    }
-    existingUser.healthPotionCount -= 1;
-    console.log('existingUser.healthPotionCount', existingUser.healthPotionCount)
-    const newPV = Math.round(Math.min(StarterPV * 1.2, existingUser.starterMAXPV));
-    await existingUser.save();
-    res.status(200).json({ message: 'Potion de soin utilisée.', newPV: newPV, nbPotion: existingUser.healthPotionCount });
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
