91e4625607e20c0bc0ac87ee1ee253f000db57e3
TzRPheonix <tzrpheonix@gmail.com>
2024-05-30 15:02:43 +0200

    protection route login/register

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -9,35 +9,44 @@ const rateLimit = require('express-rate-limit');
 
 const mongoString = process.env.DATABASE_URL;
 
-const limiter = rateLimit({
+const generalLimiter = rateLimit({
     windowMs: 60 * 1000,
     max: 300,
     message: 'Too many requests.'
-  });
+});
 
-  
+const loginLimiter = rateLimit({
+    windowMs: 60 * 1000,
+    max: 10,
+    message: 'Too many requests.'
+});
+
 const app = express();
-const port = 3000
+const port = 3000;
 
-app.use(limiter);
+app.use(generalLimiter);
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(cors());
-app.use('/api', routeur)
+
+app.use('/api/login', loginLimiter);
+app.use('/api/register', loginLimiter);
+
+app.use('/api', routeur);
 
 mongoose.connect(mongoString);
-const database = mongoose.connection
+const database = mongoose.connection;
 
-//Connexion à la database
+// Connexion à la base de données
 database.on('error', (error) => {
-    console.log(error)
-})
+    console.log(error);
+});
 
 database.once('connected', () => {
     console.log('Database Connected');
-})
+});
 
-//Démarrage du serveur
+// Démarrage du serveur
 app.listen(port, () => {
-    console.log(`Server started at https://localhost:${port}`)
-})
+    console.log(`Server started at https://localhost:${port}`);
+});
