b7f61943b0dc691ee53bdf7ea7ff3d5386f24150
TzRPheonix <tzrpheonix@gmail.com>
2024-05-30 15:18:24 +0200

    redirect login

--- BackEnd/routes/route.js
+++ BackEnd/routes/route.js
@@ -13,7 +13,7 @@ function verifyToken(req, res, next) {
   const token = req.headers.authorization;
 
   if (!token) {
-    return res.status(401).json({ message: 'Token manquant.' });
+    return res.status(401).json({ message: 'Token manquant.', redirectTo: '/login' });
   }
 
   try {
@@ -24,7 +24,7 @@ function verifyToken(req, res, next) {
     
     next();
   } catch (error) {
-    return res.status(401).json({ message: 'Token invalide ou expiré.' });
+    return res.status(401).json({ message: 'Token invalide ou expiré.', redirectTo: '/login' });
   }
 }
 
